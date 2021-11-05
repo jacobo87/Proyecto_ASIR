e24eff385eb78b9b3edf435731da7ee78518d4f2
jacobo87 <58173221+jacobo87@users.noreply.github.com>
2021-11-05 11:17:07 +0100

    Update index.php

--- src/vistas/home/index.php
+++ src/vistas/home/index.php
@@ -1,3 +1,8 @@
+<?php
+    // Añadimos una variable de inicio de sesión, si existe nos muestra la web
+    require_once("../../configuracion/conexion.php");
+    if(isset($_SESSION["id"])){
+?>
 <!DOCTYPE html>
 <html>
 <!-- Llamamos a nuestro Head -->
@@ -21,9 +26,14 @@
         </div>
         <!--.container-fluid-->
     </div>
-    <!--.page-content-->
+
     <!-- Llamamos a nuestro mainjs -->
     <?php require_once("../mainjs/js.php"); ?>
 </body>
-
 </html>
+<?php
+    // Si no, nos devuelve al index.php
+    }else{
+        header("Location:".conectar::ruta()."../../index.php");
+    }
+?>
