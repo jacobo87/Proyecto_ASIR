0a500b1a461a8fde83007fd2301baa7ea2eb6379
jacobo87 <58173221+jacobo87@users.noreply.github.com>
2021-11-05 08:59:13 +0100

    Creados require php
    Hemos creado las partes fijas de nuestra web con archivos php para darle el diseño final a la web

--- src/vistas/home/index.php
+++ src/vistas/home/index.php
@@ -1,1 +1,29 @@
-<h1>LO HAS CONSEGUIDO</h1>
+<!DOCTYPE html>
+<html>
+<!-- Llamamos a nuestro Head -->
+<?php require_once("../mainhead/head.php"); ?>
+<title>Jacobo Azmani - Home</title>
+</head>
+
+<body class="with-side-menu">
+
+    <!-- Llamamos a nuestro Header -->
+    <?php require_once("../mainheader/header.php"); ?>
+
+    <div class="mobile-menu-left-overlay"></div>
+
+    <!-- Llamamos a nuestra barra de menu -->
+    <?php require_once("../mainnav/nav.php"); ?>
+
+    <div class="page-content">
+        <div class="container-fluid">
+            Blank page.
+        </div>
+        <!--.container-fluid-->
+    </div>
+    <!--.page-content-->
+    <!-- Llamamos a nuestro mainjs -->
+    <?php require_once("../mainjs/js.php"); ?>
+</body>
+
+</html>
---
+++ src/vistas/mainnav/nav.php
@@ -0,0 +1,27 @@
+<nav class="side-menu">
+        <ul class="side-menu-list">
+            <li class="blue-dirty">
+                <a href="..\home\">
+                    <span class="glyphicon glyphicon-th"></span>
+                    <span class="lbl">Inicio</span>
+                </a>
+            </li>
+            <li class="magenta with-sub">
+                <a href="..\\">
+                <span>
+                    <span class="glyphicon glyphicon-list-alt"></span>
+                    <span class="lbl">Nuevo Caso</span>
+                </span>
+            </a>
+            </li>
+            <li class="magenta with-sub">
+                <a href="..\\">
+                <span>
+                    <span class="glyphicon glyphicon-list-alt"></span>
+                    <span class="lbl">Consultar Caso</span>
+                </span>
+            </a>
+            </li>
+        </ul>
+    </nav>
+    <!--.side-menu-->
