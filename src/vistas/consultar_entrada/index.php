e7a4e6cb4ec120ef2db74f16a718187761b676d3
jacobo87 <58173221+jacobo87@users.noreply.github.com>
2021-11-18 10:54:11 +0100

    Update index.php

--- src/vistas/consultar_entrada/index.php
+++ src/vistas/consultar_entrada/index.php
@@ -47,8 +47,6 @@ if (isset($_SESSION["id"])) {
 							<th class="d-none d-sm-table-cell" style="width: 40%;">Título</th>
 							<th class="d-none d-sm-table-cell" style="width: 5%;">Estado</th>
 							<th class="d-none d-sm-table-cell" style="width: 10%;">Fecha Creación</th>
-							<th class="d-none d-sm-table-cell" style="width: 10%;">Fecha Asignación</th>
-							<th class="d-none d-sm-table-cell" style="width: 10%;">Soporte</th>
 							<th class="text-center" style="width: 5%;"></th>
 						</tr>
 					</thead>
