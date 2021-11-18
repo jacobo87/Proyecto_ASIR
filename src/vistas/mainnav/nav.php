85353d3d9632dcd9ac2634ed70e442f6ec6e0835
jacobo87 <58173221+jacobo87@users.noreply.github.com>
2021-11-18 15:11:21 +0100

    Update nav.php
    Actualizamos la barra de navegación, si es del tipo usuario nos muestra un menú, si no nos mostrará el menú de soporte.

--- src/vistas/mainnav/nav.php
+++ src/vistas/mainnav/nav.php
@@ -1,4 +1,9 @@
-<nav class="side-menu">
+<?php
+/* Preguntamos por la sesión */
+    if ($_SESSION["rol_tipo"]=='usuario'){
+        ?>
+        <!-- Si es del tipo usuario nos muestra el siguiente menú -->
+        <nav class="side-menu">
         <ul class="side-menu-list">
             <li class="blue-dirty">
                 <a href="..\home\">
@@ -22,12 +27,29 @@
                 </span>
             </a>
             </li>
+        </ul>
+    </nav>
+    <?php
+    }else{
+        ?>
+        <!-- Si no, nos muestra el panel para el soporte -->
+        <nav class="side-menu">
+        <ul class="side-menu-list">
             <li class="blue-dirty">
-                <a href="..\..\index.php">
+                <a href="..\home\">
                     <span class="glyphicon glyphicon-th"></span>
-                    <span class="lbl">Cerrar Sesión</span>
+                    <span class="lbl">Inicio</span>
                 </a>
+            <li class="magenta with-sub">
+                <a href="..\consultar_entrada\">
+                <span>
+                    <span class="glyphicon glyphicon-list-alt"></span>
+                    <span class="lbl">Consultar Caso</span>
+                </span>
+            </a>
             </li>
         </ul>
     </nav>
-    <!--.side-menu-->
+    <?php
+    }
+?>
