13dc57beb81a4468b1fd8fd04eb5af1b89ba80e8
jacobo87 <58173221+jacobo87@users.noreply.github.com>
2021-11-19 10:42:04 +0100

    Update dbdatos.sql

--- db/dbdatos.sql
+++ db/dbdatos.sql
@@ -8,7 +8,7 @@ INSERT INTO usuarios (id, nombre, apellido1, apellido2, email, contrasenya, fech
 
 
 # Insertamos datos en la tabla categoría
-INSERT INTO `categoria` (`id`, `nombre`) VALUES 
+INSERT INTO categoria (id, nombre) VALUES 
 (1, 'Hardware'), 
 (2, 'Software'),
 (3, 'Redes'), 
@@ -17,7 +17,13 @@ INSERT INTO `categoria` (`id`, `nombre`) VALUES
 
 # Insertamos datos en la tabla caso
 INSERT INTO `caso` (`id`, `usuario_id`, `categoria_id`, `titulo`, `descripcion`, `estado`) VALUES 
-(NULL, '1', '1', 'Placa base rota.', 'PC no arranca...', '1');
+(NULL, '1', '1', 'Placa base rota.', 'PC no arranca...', '1'),
+(NULL, '3', '2', 'No funciona inicio.', 'El botón de inicio de Windows...', '1'),
+(NULL, '3', '3', 'No tengo Wi-Fi.', 'No me puedo conectar...', '1'),
+(NULL, '4', '2', 'Explorador de archivos.', 'Roto el buscador...', '1'),
+(NULL, '4', '4', 'SFC /SCANNOW.', 'Pantalla azul...', '1'),
+(NULL, '4', '1', 'USB no lo reconoce.', 'Conector roto...', '1'),
+(NULL, '3', '1', 'Placa base rota.', 'PC no arranca...', '1');
 
 # Insertamos datos en la tabla comentarios
 INSERT INTO comentarios (id, usuarios_id. caso_id, comentario, fecha_creacion_comentario) VALUES
