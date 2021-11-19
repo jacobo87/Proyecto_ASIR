f6ced5e82a08760be7cc1b40a956345ad17be4d8
jacobo87 <58173221+jacobo87@users.noreply.github.com>
2021-11-19 11:44:48 +0100

    Update caso.php

--- src/controller/caso.php
+++ src/controller/caso.php
@@ -12,15 +12,15 @@
             $caso->insert_caso($_POST["usuario_id"],$_POST["categoria_id"],$_POST["titulo"],$_POST["descripcion"]);
             break;
 
-        case "listar_usuario":
-            $datos=$caso->listar_usuario($_POST["id"]);
+        case "listar_caso":
+            $datos=$caso->listar_caso($_POST["usuario_id"]);
             $data= Array();
             foreach($datos as $row){
                 $sub_array = array();
                 $sub_array[] = $row["caso_id"];
-                $sub_array[] = $row["categoria_nombre"];
-                $sub_array[] = $row["caso_titulo"];
-                $sub_array[] = '<button type="button" onClick"ver('.$row["caso_id"].');" id="'.$row["caso_id"].' class="btn btn-inline btn-primary btn-sm ladda-button"><i class="fa fa-eye"></i></button>';
+                $sub_array[] = $row["categoria_id"];
+                $sub_array[] = $row["caso.titulo"];
+                $data[] = $sub_array;
             }
 
             $results = array(
