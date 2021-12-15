61dd2f37f388e0064c94c1ef0cf4995f65b16f14
jacobo87 <58173221+jacobo87@users.noreply.github.com>
2021-12-15 10:46:12 +0100

    Añadidos scripts para AWS

---
+++ scripts/04-create_instances.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -x
+
+# Deshabilitamos la paginación de la salida de los comandos de AWS CLI
+# Referencia: https://docs.aws.amazon.com/es_es/cli/latest/userguide/cliv2-migration.html#cliv2-migration-output-pager
+export AWS_PAGER=""
+
+# Variables de configuración
+AMI_ID=ami-0472eef47f816e45d
+COUNT=1
+INSTANCE_TYPE=t2.micro
+KEY_NAME=vockey
+
+SECURITY_GROUP=proyecto-sg
+
+INSTANCE_NAME_PROYECTO=proyecto_pi
+
+# Creamos una intancia EC2 para el balanceador de carga
+aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --count $COUNT \
+    --instance-type $INSTANCE_TYPE \
+    --key-name $KEY_NAME \
+    --security-groups $SECURITY_GROUP \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$INSTANCE_NAME_PROYECTO}]"
