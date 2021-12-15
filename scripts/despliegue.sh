316e347d3b92a46a7c9fc95eb368d38f30194c8e
jacobo87 <58173221+jacobo87@users.noreply.github.com>
2021-12-15 11:42:16 +0100

    Update

--- scripts/despliegue.sh
+++ scripts/despliegue.sh
@@ -10,16 +10,17 @@ apt update
 apt install docker -y
 apt install docker-compose -y
 
-
+# Introducimos a docker dentro del grupo de usuarios
 usermod -aG docker $USER
 newgrp docker
 
-#
+# Habilitamos Docker
 systemctl enable docker 
 systemctl start docker
 
-
+# Clonamos el repositorio
 git clone https://github.com/jacobo87/Proyecto_ASIR
 
+# Accedemos al proyecto y lo desplegamos
 cd Proyecto_ASIR
 docker-compose up -d
